c39ae8367d020f685b819dfadba118e58542e8ec
wereh <wereh.manuh@gmail.com>
2024-09-23 13:21:30 +0300

    connect to DB

--- api/server.js
+++ api/server.js
@@ -1,10 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Connect to MongoDB
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 const app = express();
-
 
 //listen on http://localhost:5000
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000')
+  console.log("Server started on port 3000");
 });
